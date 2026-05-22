2207801eeaf6385dd4b9927119ed172cd09f1b46
Aiana Kulnazarova <aiana.kulnazarova@alatoo.edu.kg>
2026-05-22 11:43:36 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: 'all',
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
   preview: {
-    allowedHosts: 'all'
-  }
+    allowedHosts: 'all',
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+  },
 })
