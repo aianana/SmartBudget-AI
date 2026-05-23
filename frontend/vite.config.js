725233355d4710f6a8c5066dbd86c0665e02104e
Aiana Kulnazarova <aiana.kulnazarova@alatoo.edu.kg>
2026-05-23 12:58:15 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
-    },
-    server: {
-      host: '0.0.0.0',
-    },
-    preview: {
-      allowedHosts: 'all',
-      host: '0.0.0.0',
-    }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
+  preview: {
+    allowedHosts: 'all',
+    host: '0.0.0.0',
   }
 })
-
